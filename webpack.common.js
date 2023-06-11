0b04e24be24bbd07cdcbafaeb6256347192e0a83
Daffa Naufal Santoso <Naufalsdaffa75@gmail.com>
2023-06-11 09:08:45 +0700

    menambahkan pwa & web pformnace

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -13,6 +14,29 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
   module: {
     rules: [
@@ -60,8 +84,45 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+          globOptions: {
+            ignore: ['**/*.webp'],
+          },
+        },
+        {
+          from: path.resolve(__dirname, 'src/public/images'),
+          to: path.resolve(__dirname, 'dist/images'),
         },
       ],
-    }),
+    }),    
+    new ImageminWebpWebpackPlugin({
+      config: [
+        {
+          test: /\.(jpe?g|png)$/,
+          path: path.resolve(__dirname, 'src/public/images'),
+          destination: [
+            {
+              path: path.resolve(__dirname, 'dist/images'),
+              options: {
+                quality: 75,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(jpe?g|png)$/,
+          path: path.resolve(__dirname, 'src/public/icons'),
+          destination: [
+            {
+              path: path.resolve(__dirname, 'dist/icons'),
+              options: {
+                quality: 80,
+              },
+            },
+          ],
+        },
+      ],
+      overrideExtension: true,
+      strict: false,
+    }),    
   ],
 };
